464c9d88d46be287bf76964558295448d8aad626
Isabela <icrr1@aluno.ifnmg.edu.br>
2026-05-25 07:32:52 -0300

    Entrega do modulo Front-end do SaaS

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/info3/isabela/projeto_saas/venv
+set -gx VIRTUAL_ENV /home/info3/projeto_saas/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
